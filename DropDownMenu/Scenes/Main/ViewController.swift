63fe36bbcefdbe16e9ffacc6a4cfe7cbdd92258c
Nicolò Curioni <nicolo.curioni96@gmail.com>
2022-04-05 09:54:44 +0200

    Added a button to the main scene `ViewController`

--- DropDownMenu/Scenes/Main/ViewController.swift
+++ DropDownMenu/Scenes/Main/ViewController.swift
@@ -9,15 +9,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var buttonOption: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupUI()
     }
     
+    // MARK: - Methods
+    
+    // Private
     private func setupUI() {
         title = "DropDownMenu"
         view.backgroundColor = UIColor(named: "MainBG")
+        
+        buttonOption.backgroundColor = UIColor(named: "NormalButton")
+        buttonOption.semanticContentAttribute = .forceLeftToRight
+        buttonOption.setTitle("Select option", for: .normal)
+        buttonOption.clipsToBounds = true
+        buttonOption.layer.cornerRadius = 8
+        buttonOption.setTitleColor(.black, for: .normal)
+        buttonOption.layer.shadowColor = UIColor.black.cgColor
+        buttonOption.layer.shadowOffset = CGSize(width: -4, height: 3)
+        buttonOption.layer.shadowOpacity = 0.6
+        buttonOption.addTarget(self, action: #selector(didTapOnSelectOption(_:)), for: .touchUpInside)
+        
+        setupConstraints()
     }
+    
+    private func setupConstraints() {
+        
+        [buttonOption].forEach(view.addSubview(_:))
+        
+        NSLayoutConstraint.activate([
+            buttonOption.heightAnchor.constraint(equalToConstant: 52),
+            buttonOption.widthAnchor.constraint(equalToConstant: 200),
+            buttonOption.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            buttonOption.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+        ])
+    }
+    
+    @objc
+    private func didTapOnSelectOption(_ sender: Any) { }
 }
 
