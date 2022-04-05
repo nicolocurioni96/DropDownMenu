516895ea1880146bce7bf5a10beda44bb9cc2882
Nicolò Curioni <nicolo.curioni96@gmail.com>
2022-04-05 09:49:13 +0200

    Added the `ButtonImageName` enum, tha will contain all button image name, as String

---
+++ DropDownMenu/Enums/Image+Enums.swift
@@ -0,0 +1,13 @@
+//
+//  Image+Enums.swift
+//  DropDownMenu
+//
+//  Created by Nicolò Curioni on 05/04/22.
+//
+
+import Foundation
+
+public enum ButtonImageName: String, CaseIterable {
+    case arrowTop = "ArrowTop"
+    case arrowBottom = "ArrowBottom"
+}
